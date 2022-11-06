3b6fa2e2c04e8e9b753d7cfce1339f2327aa50f0
Cleopatra Popescu <melody.nickname@gmail.com>
2022-11-06 20:47:43 +0200

    Added Node.cs and base for Huffman Tree generation.

---
+++ HuffTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanEncoder
+{
+    public class HuffTree
+    {
+        private List<Node> Nodes;
+        private Node Root;
+        private Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public HuffTree(String input)
+        {
+            Nodes = new List<Node>();
+            foreach (char c in input)
+            {
+                if (!Frequencies.ContainsKey(c))
+                    Frequencies.Add(c, 0);
+                Frequencies[c]++;
+            }
+
+            foreach (KeyValuePair<char, int> entry in Frequencies)
+            {
+                Node node = new Node(entry.Key, entry.Value);
+                Nodes.Add(node);
+            }
+
+            while (Nodes.Count > 1)
+            {
+                List<Node> nodes = Nodes.OrderBy(node => node.Frequency_).ToList<Node>();
+                if (nodes.Count > 1)
+                {
+                    Node left = nodes[0];
+                    Node right = nodes[1];
+
+                    Node new_node = new Node('~', left.Frequency_ + right.Frequency_);
+                    new_node.Right_ = right;
+                    new_node.Left_ = left;
+
+                    Nodes.Add(new_node);
+                }
+
+                Root = Nodes.FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanEncoder
+{
+    public class Node
+    {
+        private Node Left;
+        private Node Right;
+
+        private char Character;
+        private int Frequency;
+
+        public char Character_ { get => Character; set => Character = value; }
+        public int Frequency_ { get => Frequency; set => Frequency = value; }
+        public Node Left_ { get => Left; set => Left = value; }
+        public Node Right_ { get => Right; set => Right = value; }
+
+        public Node() {}
+
+        public Node(char character, int frequency)
+        {
+            Character = character;  
+            Frequency = frequency;
+        }
+    }
+}
